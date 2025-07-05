bc239c905fccb48d95d19752e7f797d6c6d50ef1
KaanIsmet <kaanismet7@gmail.com>
2025-07-05 14:50:41 -0500

    added configurations

---
+++ src/config.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://www.microcenter.com/search/search_results.aspx?N=&cat=&Ntt="
+SEARCH_ENDPOINT = "search/"
+SEARCH_RESULTS_ENDPOINT = "search_results.aspx?N&cat=&Ntt="
+
+#modify item searched
+#use '+' as delimiter
+ITEM_SEARCHED = "ryzen+7+cpu"
