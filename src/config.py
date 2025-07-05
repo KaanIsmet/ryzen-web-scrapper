87914ba1235c6a73ea6ca67ccd9a3209fd5b9930
KaanIsmet <kaanismet7@gmail.com>
2025-07-05 14:55:38 -0500

    added more to configurations

--- src/config.py
+++ src/config.py
@@ -5,3 +5,11 @@ SEARCH_RESULTS_ENDPOINT = "search_results.aspx?N&cat=&Ntt="
 #modify item searched
 #use '+' as delimiter
 ITEM_SEARCHED = "ryzen+7+cpu"
+
+FILE_EXTENSION = "csv"
+OUTPUT_FILE = "scraped_data.csv"
+
+#tell parser where to find data on webpage
+SELECTORS = {
+
+}
