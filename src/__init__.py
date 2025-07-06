ca42b0e610f3429f5a4614c218da03c577d79aa3
KaanIsmet <kaanismet7@gmail.com>
2025-07-06 10:56:29 -0500

    fixed bug where url and endpoints weren't compiling in main

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,4 @@ __version__ = "1.0.0"
 
 from .scraper import Scraper
 from .parser import Parser
-from .config import SELECTORS
+from .config import SELECTORS, BASE_URL, SEARCH_ENDPOINT, SEARCH_RESULTS_ENDPOINT, CATEGORY_CONFIG
