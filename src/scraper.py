7cb6befd5d33fc04a11a88f3ab94f7de55bddf7c
KaanIsmet <kaanismet7@gmail.com>
2025-07-06 22:34:03 -0500

    edited scraper

--- src/scraper.py
+++ src/scraper.py
@@ -3,7 +3,6 @@ import requests
 def Scraper(base_url: str, search_endpoint: str, search_results_endpoint: str, item_searched: str,category_config: str):
 
     url = base_url + search_endpoint + search_results_endpoint + item_searched + category_config
-    print(url)
     response = requests.get(url)
 
     if response.ok:
